3cd34bd60164bcac1f5d853862bdda468ea6268b
rgero <rgero@student.21-school.ru>
2020-02-25 17:28:02 +0300

    refact

--- src/ft_put_piece.c
+++ src/ft_put_piece.c
@@ -6,11 +6,35 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 16:26:58 by rgero             #+#    #+#             */
-/*   Updated: 2020/02/25 15:03:29 by rgero            ###   ########.fr       */
+/*   Updated: 2020/02/25 17:27:30 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
+
+int		ft_get_sum_max(t_map *map)
+{
+	int ret;
+	int	i;
+	int	j;
+
+	ret = 0;
+	i = 0;
+	while (i < map->piece_height)
+	{
+		j = 0;
+		while (j < map->piece_width)
+		{
+			if ('*' == map->piece[i][j])
+				ret++;
+			j++;
+		}
+		i++;
+	}
+	ret *= (map->map_height > map->map_width\
+		? map->map_height : map->map_width);
+	return (ret);
+}
 
 int		ft_get_sum(t_map *map, int h, int w)
 {
@@ -68,13 +92,14 @@ void	ft_set_piece(t_map *map, int sum, int h, int w)
 	map->piece_w = w;
 }
 
+
 void		ft_put_piece(t_map *map)
 {
 	int	h;
 	int	w;
 	int	sum;
 
-	map->piece_min = 1000;
+	map->piece_min = ft_get_sum_max(map);
 	h = 0;
 	while (h + map->piece_height <= map->map_height)
 	{
